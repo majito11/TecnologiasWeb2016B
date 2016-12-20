0cf55036b75d0efbe1aacf58c5dc784609be25be
majito11 <majosita93@hotmail.com>
2016-12-20 15:17:11 -0500

    1

---
+++ ServidorHTTP/mascotas/api/controllers/SaludoController.js
@@ -0,0 +1,11 @@
+/**
+ * SaludoController
+ *
+ * @description :: Server-side logic for managing Saludoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
