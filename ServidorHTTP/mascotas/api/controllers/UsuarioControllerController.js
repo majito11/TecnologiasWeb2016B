6de3c5e97311540fcd667e8ed5e4f8d59aafa49d
majito11 <majosita93@hotmail.com>
2016-12-21 13:43:32 -0500

    1

---
+++ ServidorHTTP/mascotas/api/controllers/UsuarioControllerController.js
@@ -0,0 +1,11 @@
+/**
+ * UsuarioControllerController
+ *
+ * @description :: Server-side logic for managing Usuariocontrollers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
